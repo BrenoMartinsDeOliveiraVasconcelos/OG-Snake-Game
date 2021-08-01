793f0057dc0133854ed56f66e044ad5cc2b33876
Breno Martins <67431981+BrenoMartinsDeOliveiraVasconcelos@users.noreply.github.com>
2021-07-31 21:32:45 -0300

    Fxed issue
    On Linux, the game doesn't start because line 7 was requesting "ARCADE.tff" when it doesn't exists for Linux, this can be solved changing the "tff" for "TFF"

--- theme_settings/font_registry.py
+++ theme_settings/font_registry.py
@@ -4,4 +4,4 @@ import dearpygui.dearpygui as dpg
 with dpg.font_registry() as main_font_registry:
     regular_font = dpg.add_font('fonts/Roboto/Roboto-Regular.ttf', 16, default_font=True)
     bold_font = dpg.add_font('fonts/Roboto/Roboto-Bold.ttf', 21)
-    score_font = dpg.add_font('fonts/ARCADE.ttf', 35)
+    score_font = dpg.add_font('fonts/ARCADE.TTF', 35)
